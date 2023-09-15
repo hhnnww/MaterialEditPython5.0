2767c57994e239f68bb04e78babd14e34c354d19
weihua <175990271@qq.com>
2023-09-15 10:24:39 +0800

    123

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from router.router_制作详情 import router as router_制作详情
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(router=router_制作详情)
